4d7e8a2d31592defbd879ec81748e3b8a34ed580
Juan Gabriel Hernando <juangabriel.hernando@hotmail.com>
2024-11-05 09:27:07 -0300

    tercer commit

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,44 @@
+// Verifica si es la primera visita
+if (!localStorage.getItem("primeraVisita")) {
+
 /*solicitud de Nombre*/
-/*
-let ingresarNombre = prompt("ingrese su nombre");
 
-alert("!Hola" + " " + ingresarNombre + "!");
+let nombre;
 
-*/
+while (true) {
+  nombre = prompt("Por favor, ingresa tu nombre:");
 
+  if (isNaN(nombre)) {
+    alert("Bienvenido" + " " + nombre + "!");
+    break;
+  } else {
+    alert("Error: Por favor, ingresa un nombre válido (sin números).");
+  }
+}
+
+/* pregunta el presupuesto y segun el valor ingresado le dice cuantos articulos puede compra*/
+
+let presupuesto;
+
+while (true){
+
+    presupuesto = prompt("ingresa tu presupuesto")
+
+    if (isNaN(presupuesto)) {
+        alert("Error: Por favor, ingresa un numero válido.");
+        
+    }
+        else if (presupuesto < 15000){
+          alert("No tenes suficiente dinero para comprar, vuelve mas tarde")
+        break
+        }
+        else if (presupuesto >! 15000 ){
+            alert("bienvenido a nuestra tienda!")
+            break
+        }
+        
+    
+    }
+    
+    localStorage.setItem("primeraVisita", "true");
+}
